771209b5b47d359a052c7f03505810757921a205
ashwinik-cognologix <ashwini.khairnar@cognologix.com>
2022-12-06 11:30:25 +0530

    strapi backend api

---
+++ src/api/orderdetail/controllers/orderdetail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orderdetail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::orderdetail.orderdetail');
---
+++ src/api/orderdetail/services/orderdetail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orderdetail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::orderdetail.orderdetail');
